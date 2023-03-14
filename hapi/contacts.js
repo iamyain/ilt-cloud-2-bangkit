b6c6aa7f664c87dea1fdf76760f5beeeff647fe1
iamyain <nisrinaathiyya@gmail.com>
2023-03-14 16:36:36 +0700

    Contoh implementasi Restfull API

---
+++ hapi/contacts.js
@@ -0,0 +1,12 @@
+const contacts = [
+    {
+      id: 1,
+      name: 'Nisrina',
+      email: 'Athiyya@mail.com',
+      phone: "123200042",
+    },
+  ];
+  
+  
+  module.exports = contacts;
+  
---
+++ hapi/server.js
@@ -0,0 +1,50 @@
+const Hapi = require('@hapi/hapi');
+const contacts = require("./contacts");
+
+(async () => {
+  const server = Hapi.server({
+    port: 3000,
+    host: 'localhost',});
+
+  server.route([
+    {
+      method: 'POST',
+      path: '/contacts',
+      handler: (request, h) => {
+        const { nama, email, nim } = request.payload;
+        const id = contacts[contacts.length - 1].id + 1;
+        contacts.push({
+          id,
+          nama,
+          email,
+          nim
+        });
+        const response = h.response({ message: 'Kontak berhasil ditambahkan' });
+        response.code(201);
+        return response;
+      },
+    },
+    {
+        method: 'DELETE',
+        path: '/contacts/{id}',
+        handler: (request, h) => {
+          const { id } = request.params;
+          const index = contacts.findIndex(contact => contact.id === Number(id));
+          if (index === -1) {
+            const response = h.response({ message: 'Kontak tidak ditemukan' });
+            response.code(404);
+            return response;
+          }
+          contacts.splice(index, 1);
+          return { message: 'Kontak berhasil dihapus' };
+        },
+      },
+    {
+      method: 'GET',
+      path: '/contacts',
+      handler: () => contacts,
+    },
+  ]);
+  await server.start();
+  console.log('Server berjalan di %s', server.info.uri);
+})();
