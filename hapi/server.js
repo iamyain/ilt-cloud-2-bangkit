4f4e41c93f31d9d567c84fa1663b356ab2c48b11
iamyain <115709410+iamyain@users.noreply.github.com>
2023-03-14 16:53:58 +0700

    Update server.js

--- hapi/server.js
+++ hapi/server.js
@@ -1,50 +1,49 @@
 const Hapi = require('@hapi/hapi');
 const contacts = require("./contacts");
 
-(async () => {
-  const server = Hapi.server({
-    port: 3000,
-    host: 'localhost',});
 
-  server.route([
-    {
-      method: 'POST',
-      path: '/contacts',
-      handler: (request, h) => {
-        const { nama, email, phone } = request.payload;
-        const id = contacts[contacts.length - 1].id + 1;
-        contacts.push({
-          id,
-          nama,
-          email,
-          phone
-        });
-        const response = h.response({ message: 'Kontak berhasil ditambahkan' });
-        response.code(201);
-        return response;
-      },
-    },
-    {
-        method: 'DELETE',
-        path: '/contacts/{id}',
-        handler: (request, h) => {
-          const { id } = request.params;
-          const index = contacts.findIndex(contact => contact.id === Number(id));
-          if (index === -1) {
-            const response = h.response({ message: 'Kontak tidak ditemukan' });
-            response.code(404);
-            return response;
-          }
-          contacts.splice(index, 1);
-          return { message: 'Kontak berhasil dihapus' };
-        },
-      },
-    {
-      method: 'GET',
-      path: '/contacts',
-      handler: () => contacts,
-    },
-  ]);
-  await server.start();
-  console.log('Server berjalan di %s', server.info.uri);
+const server = Hapi.server({
+  port: 3000,
+  host: 'localhost',
+});
+
+server.route({
+  method: 'POST',
+  path: '/contacts',
+  handler: (request, h) => {
+    const contact = request.payload;
+    contacts.push(contact);
+    return h.response(contact).code(201);
+  },
+});
+
+server.route({
+  method: 'GET',
+  path: '/contacts',
+  handler: (request, h) => {
+    return h.response(contacts).code(200);
+  },
+});
+
+server.route({
+  method: 'DELETE',
+  path: '/contacts/{id}',
+  handler: (request, h) => {
+    const id = request.params.id;
+    const index = contacts.findIndex(c => c.id === id);
+    if (index !== -1) {
+      contacts.splice(index, 1);
+    }
+    return h.response().code(200);
+  },
+});
+
+(async () => {
+  try {
+    await server.start();
+    console.log(`Server running at: ${server.info.uri}`);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
 })();
